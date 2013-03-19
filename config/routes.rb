a1f539317823cbef03f9c8af20e2968716f82092
John Norman <john@7fff.com>
2013-03-18 20:22:29 -0500

    Add API

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,12 @@ ExOauthProvider::Application.routes.draw do
 
   devise_for :users
 
+  namespace :api do
+    namespace :v1 do
+      match "data" => "data#show"
+    end
+  end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
