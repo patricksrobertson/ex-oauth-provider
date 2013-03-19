b206d2f815c125ac714351882d14ea7d5df2bb63
John Norman <john@7fff.com>
2013-03-18 19:34:55 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExOauthProvider::Application.config.secret_token = '5896da9d7ec336bdad152b092066c49af32f51e93bd7ff80d6a872bde911e43e4d3648ea97644c70439a4416530773d98ccca3b81ffa60748a5992ab71a07dfe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ExOauthProvider::Application.config.session_store :cookie_store, key: '_ex-oauth-provider_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ExOauthProvider::Application.config.session_store :active_record_store
