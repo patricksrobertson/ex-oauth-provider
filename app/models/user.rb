856c426d8e19d67f664e44dffcbc2c8dba5b88fe
Patrick Robertson <patricksrobertson@gmail.com>
2013-03-22 09:01:20 -0400

    Add relations for the User class.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  has_many :client_applications
+  has_many :tokens, through: :client_applications, class_name: 'OauthToken'
 end
